cf60f44a69e17a7433cad3a3bd070a55bfab87cb
Marwan Khaled <160495968+marwan5aled26@users.noreply.github.com>
2024-04-12 13:14:19 +0200

    Flip Image.cpp

--- Flip Image.cpp
+++ Flip Image.cpp
@@ -1,6 +1,27 @@
 #include "Image_Class.h"
-#include <iostream>
 using namespace std;
+
+void flipImage(Image& image, const string& choose) {
+    if (choose == "1") { // Horizontal Flip
+        for (int i = 0; i < image.width / 2; i++) {
+            for (int j = 0; j < image.height; j++) {
+                for (int k = 0; k < image.channels; k++) {
+                    // Flip pixels from right to left.
+                    swap(image.getPixel(i, j, k), image.getPixel(image.width - i - 1, j, k));
+                }
+            }
+        }
+    } else if (choose == "2") { // Vertical Flip
+        for (int i = 0; i < image.width; i++) {
+            for (int j = 0; j < image.height / 2; j++) {
+                for (int k = 0; k < image.channels; k++) {
+                    // Flip pixels from up to down.
+                    swap(image.getPixel(i, j, k), image.getPixel(i, image.height - j - 1, k));
+                }
+            }
+        }
+    }
+}
 
 int main() {
     string filename;
@@ -17,27 +38,7 @@ int main() {
         getline(cin, choose);
     }
 
-    if (choose == "1") { // Horizontal Flip
-        for (int i = 0; i < image.width / 2; i++) {
-            for (int j = 0; j < image.height; j++) {
-                for (int k = 0; k < image.channels; k++) {
-                    // Flip pixels from right to left.
-                    swap(image.getPixel(i, j, k), image.getPixel(image.width - i - 1, j, k));
-                }
-            }
-        }
-    }
-
-    else if (choose == "2") { // Vertical Flip
-        for (int i = 0; i < image.width; i++) {
-            for (int j = 0; j < image.height / 2; j++) {
-                for (int k = 0; k < image.channels; k++) {
-                    // Flip pixels from up to down.
-                    swap(image.getPixel(i, j, k), image.getPixel(i, image.height - j - 1, k));
-                }
-            }
-        }
-    }
+    flipImage(image, choose);
 
     cout << "Please enter the image name to store the new image and specify the extension (.jpg, .bmp, .png, .tga): ";
     cin >> filename;
